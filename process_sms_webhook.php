f43b328dfe02afbec254be5f13289d1a72da3be6
pbmacintyre <pbmacintyre@gmail.com>
2025-07-17 12:33:03 -0300

    Tweaks while writing blog text

--- process_sms_webhook.php
+++ process_sms_webhook.php
@@ -14,7 +14,7 @@ require_once('includes/ringcentral-php-functions.inc');
 
 $hvt = isset($_SERVER['HTTP_VALIDATION_TOKEN']) ? $_SERVER['HTTP_VALIDATION_TOKEN'] : '';
 if (strlen($hvt) > 0) {
-    header("Validation-Token: {$hvt}");
+	header("Validation-Token: {$hvt}");
 }
 
 $incoming = file_get_contents("php://input");
@@ -23,17 +23,17 @@ $incoming = file_get_contents("php://input");
 file_put_contents("received_SMS_EVENT_payload.log", $incoming);
 
 if (empty($incoming)) {
-    http_response_code(200);
-    echo json_encode(array('responseType' => 'error', 'responseDescription' => 'No data provided Check SMS payload.'));
-    exit();
+	http_response_code(200);
+	echo json_encode(array('responseType' => 'error', 'responseDescription' => 'No data provided Check SMS payload.'));
+	exit();
 }
 
 $incoming_data = json_decode($incoming);
 
 if (!$incoming_data) {
-    http_response_code(200);
-    echo json_encode(array('responseType' => 'error', 'responseDescription' => 'Media type not supported.  Please use JSON.'));
-    exit();
+	http_response_code(200);
+	echo json_encode(array('responseType' => 'error', 'responseDescription' => 'Media type not supported.  Please use JSON.'));
+	exit();
 }
 
 // parse out the incoming information
@@ -44,9 +44,9 @@ $toNumber = htmlentities($incoming_data->body->from->phoneNumber);
 // our mobile number
 $fromNumber = htmlentities($incoming_data->body->to[0]->phoneNumber);
 
-echo_spaces("SMS Subject", $incoming_sms,1);
-echo_spaces("To Number", $toNumber,1);
-echo_spaces("From Number", $fromNumber,1);
+echo_spaces("SMS Subject", $incoming_sms, 1);
+echo_spaces("To Number", $toNumber, 1);
+echo_spaces("From Number", $fromNumber, 1);
 
 if (preg_match('/^(STOP)$/i', $incoming_sms)) {
 	send_stop_sms($toNumber);
--- sms_reminder_sending_cron.php
+++ sms_reminder_sending_cron.php
@@ -9,18 +9,18 @@ show_errors();
 
 $message_array = array();
 $today = date("Y-m-d");
-$i = 0 ;
+$i = 0;
 
 // get all reminders for today
 $table = "events";
-$columns_data = "*" ;
-$where_info = array ("reminder_date", $today);
+$columns_data = "*";
+$where_info = array("reminder_date", $today);
 $event_reminders_db_result = db_record_select($table, $columns_data, $where_info);
 
 // now find all reminders that we set for clients
 foreach ($event_reminders_db_result as $value) {
 	$table = "reminders";
-	$columns_data = array ("client_id", );
+	$columns_data = array("client_id",);
 	$where_info = array("event_id", $value['event_id']);
 	$reminders_db_result = db_record_select($table, $columns_data, $where_info);
 
@@ -34,9 +34,9 @@ foreach ($event_reminders_db_result as $value) {
 			$client_db_result = db_record_select($table, $columns_data, $where_info);
 
 			$message = "Dear " . $client_db_result[0]['first_name'] . " " . $client_db_result[0]['last_name'] . ": ";
-			$message .= "You have requested that we send you a reminder on the following event information - " ;
-			$message .=	"Event Summary: '" . $value['event_summary'] . "' ";
-			$message .= "Event Date: " . date("M d, Y", strtotime($value['event_date']) ). " " ;
+			$message .= "You have requested that we send you a reminder on the following event information - ";
+			$message .= "Event Summary: '" . $value['event_summary'] . "' ";
+			$message .= "Event Date: " . date("M d, Y", strtotime($value['event_date'])) . " ";
 			$message .= "Event Details: '" . $value['event_deets'] . "' REPLY STOP to discontinue receiving SMS messages from this app. ";
 
 			// add to message array
@@ -47,9 +47,11 @@ foreach ($event_reminders_db_result as $value) {
 	}
 }
 
+$controller = ringcentral_sdk();
+
 // now send out the SMS messages.
 foreach ($message_array as $sms_info) {
-	send_sms($sms_info["mobile"], $sms_info["message"]);
+	send_sms($controller, $sms_info["mobile"], $sms_info["message"]);
 }
 
 // since this is non-visual code, write into the log if desired to show that CRON ran
